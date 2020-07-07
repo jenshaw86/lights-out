fd3205d894a85f69be248a97126a620d671bde9e
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-07-06 21:17:39 -0700

    Add TODO comments

--- src/components/game/Tile.js
+++ src/components/game/Tile.js
@@ -3,3 +3,6 @@ import React from 'react';
 const Tile = props => <div>Tile {props.idx}</div>
 
 export default Tile;
+
+// TODO Add styling sheet - default background color (lights on)
+// TODO Add logic for random lights
