671f7ff48d1018ba002c8df8828a7c77f59fd92f
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-07-07 09:16:10 -0700

    Board and tile styling to create default lights on grid

--- src/components/game/Tile.js
+++ src/components/game/Tile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Tile.css';
 
-const Tile = props => <div className="tile">Tile {props.idx}</div>
+const Tile = props => {
+    return (
+        <div className="tile">Tile {props.idx}</div>
+    )
+}
 
 export default Tile;
 
-// TODO Add styling sheet - default background color (lights on)
 // TODO Add logic for random lights
