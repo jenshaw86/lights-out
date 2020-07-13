520c3269ffd7937288f416af6341ea1190f7b041
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-07-12 17:10:45 -0700

    Add toggledTile state and activateTiles method to set toggleTiles state

--- src/components/game/Board.js
+++ src/components/game/Board.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tile from './Tile';
 import './Board.css';
 
 const Board = props => {    
+    const [toggledTiles, setToggledTiles] = useState([]);
 
     const size = 5;
     const tileCount = Math.pow(size, 2);
+
+    const activateTiles = tileArray => {
+        setToggledTiles(tileArray);
+    }
 
     const populateTiles = () => {
         let tileArray = [];
         
         for(let i = 1; i <= tileCount; i++) {
-            tileArray.push(<Tile key={i} idx={i} gameOver={props.gameOver} turnCount={props.turnCount} setTurnCount={props.setTurnCount}/>)
+            tileArray.push(<Tile key={i} idx={i} gameOver={props.gameOver} turnCount={props.turnCount} setTurnCount={props.setTurnCount} activateTiles={activateTiles} toggledTiles={toggledTiles}/>)
         }
         return tileArray;
     }
