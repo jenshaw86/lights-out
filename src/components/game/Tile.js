1cf7fcd697172f738cea6e217fcb67bd9f09a51e
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-07-12 17:33:41 -0700

    Comments and refactoring

--- src/components/game/Tile.js
+++ src/components/game/Tile.js
@@ -15,22 +15,22 @@ const Tile = props => {
     const ABOVE_TILE = idx - 5;
     const BELOW_TILE = idx + 5;
 
+    // sets the board tiles when game starts
     useEffect(() => {
         if (!props.gameOver) {
-            console.log("Set game");
             Math.random() <= 0.5 ? setTileClass("tile-on") : setTileClass("tile-off")
         } else {
-            console.log("Close game");
             setTileClass("tile-on");
         }
     }, [props.gameOver]);
 
+    // toggles tiles surrounding clicked tile
     useEffect(() => {
         props.toggledTiles.forEach(tile => toggleTile(tile))
     }, [props.toggledTiles]);
 
     const toggleTile = tile => {
-        if (tile === props.idx) {
+        if (tile === idx) {
             tileClass === "tile-on" ? setTileClass("tile-off") : setTileClass("tile-on");   
         }
     }
@@ -46,17 +46,15 @@ const Tile = props => {
 
     const handleClick = () => {
         if (props.gameOver) {
-            console.log("clickety-click!!");
             props.setTurnCount(props.turnCount + 1);
-            let tiles = getSurroundingTiles(props.idx);
+            
+            let tiles = getSurroundingTiles(idx);
             props.activateTiles(tiles);
-        } else {
-            console.log("MEH");
         }
     }
 
     return (
-        <div className={"tile " + tileClass} onClick={handleClick}>Tile {props.idx}</div>
+        <div className={"tile " + tileClass} onClick={handleClick}>Tile {idx}</div>
     )
 }
 
